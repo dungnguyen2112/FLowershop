7e7466cea7e3d2bc09600c3607c966d2bb4ea809
dungnguyen2112 <you@example.com>
2024-09-07 00:11:59 +0700

    flowershop

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from Flowershop.center.database import Base, engine
 from Flowershop.center import models, routers
 from Flowershop.center.routers import products, customers, orders, revenuedate, auth, admin
 
-# Create tables if they don't exist
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Include your routers
 app.include_router(customers.router)
 app.include_router(orders.router)
 app.include_router(revenuedate.router)
@@ -17,9 +15,6 @@ app.include_router(auth.router)
 app.include_router(admin.router)
 
 
-
-
-# Example route
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Flower Shop API"}
